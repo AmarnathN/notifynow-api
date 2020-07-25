0f8561f05eda35afb33799569098edfc2f1d1c03
AmarnathN <vayuteja@gmail.com>
2020-07-25 23:53:05 +0530

    Updated the Notify for Prime video mail

--- app/notify/views.py
+++ app/notify/views.py
@@ -1,6 +1,6 @@
 from rest_framework import views
 from rest_framework.response import Response
-from core.models import User, UserMail, ForwardMailId
+from core.models import User, UserMail, ForwardMailId, Consent
 from twilio.rest import Client
 import json
 import sys
@@ -20,9 +20,8 @@ class NotifyView(views.APIView):
             sender = data.get("sender")
             print("Sender : {} ".format(sender))
             print(mail_subject)
-            SECRET_KEY = os.getenv("ENV_KEY")
-            print(SECRET_KEY)
             users = list(User.objects.filter(email=sender).values())
+            
             if("Gmail Forwarding Confirmation" in mail_subject  ):
                 subject_array = mail_subject.split()
                 email_from = subject_array[ len(subject_array) - 1 ]
@@ -59,8 +58,10 @@ class NotifyView(views.APIView):
             html_data = data.get('stripped-html')
             soup = BeautifulSoup(html_data,'html.parser')
 
+            notify_on_whatsApp = False
+
             if "is now on Netflix".lower() in mail_subject.lower():
-                # print(soup.prettify())
+
                 header_elem = soup.select('[id*="-header-copy"]')
                 profile = header_elem[0].get_text().strip().replace("For ","")
                 print("profile : {}".format(profile))
@@ -73,23 +74,53 @@ class NotifyView(views.APIView):
                 notifcation_img = main_link_elem.find('img')['src']
                 print("notifcation_img : {}".format(notifcation_img))
 
-                message_body = "{} \n\nPlease find the Show @ \n\nDear {},\n{}".format(mail_subject,profile, notifcation_link)
-                client = Client(account_sid, auth_token)
+                message_body = "Dear {},\n{} \n\nPlease find the Netflix Show @ \n\n{}".format(profile,mail_subject,notifcation_link)
+                notify_on_whatsApp = True
+            
+            if "recently added on Prime Video".lower() in mail_subject.lower():
                 
-                message = client.messages.create(
-                    media_url=[notifcation_img],
-                    from_="whatsapp:+14155238886",
-                    body=(message_body),
-                    to="whatsapp:{}".format(user.phone_number),
-                )
-                print(
-                    "notify mail from netflix has been whatsapped to {} !!! and mail saved to user mails".format(
-                        sender
+                mail_subject_elem = mail_subject.split(',')
+                profile = mail_subject_elem[0]
+                print("profile : {}".format(profile))
+
+                main_link_elem = soup.select('a[href*="HE_1"]')[0]
+                notifcation_link = main_link_elem['href']
+                print("notifcation_link : {}".format(notifcation_link))
+
+                # main_img_elem = soup.select('[class*="-component-image-cell"] img')[0]
+                notifcation_img = main_link_elem.find('img')['src']
+                print("notifcation_img : {}".format(notifcation_img))
+
+                message_body = "Dear {},\n{} \n\nPlease find the Prime Video Show @ \n\n{}".format(profile,mail_subject_elem[len(mail_subject_elem) - 1], notifcation_link)
+                notify_on_whatsApp = True
+            
+            
+            if notify_on_whatsApp:
+
+                user_whatsapp_consent =  Consent.objects.get(user=user).whatsapp
+
+                if user_whatsapp_consent:
+                    client = Client(account_sid, auth_token)
+                    message = client.messages.create(
+                        media_url=[notifcation_img],
+                        from_="whatsapp:+14155238886",
+                        body=(message_body),
+                        to="whatsapp:{}".format(user.phone_number),
                     )
-                )
+                    print(
+                        "notify has been whatsapped to {} !!! and mail saved to user mails".format(
+                            sender
+                        )
+                    )
+                else:
+                    print(
+                        "notify whatsApp has been disabled for user -  {} !!! \n hence no notification sent and mail saved to user mails".format(
+                            sender
+                        )
+                    )
             else:
                 print(
-                    "not valid notify mail from netflix, saved in {} mails".format(
+                    "not valid notify mail format, saved in {} mails".format(
                         userMail.user.email
                     )
                 )
